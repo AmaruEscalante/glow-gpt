d736229e245e6ee6b46c9f88eb5f780cd63b2017
SebastianHurtado98 <sebastian.hurtado@utec.edu.pe>
2023-09-22 22:20:43 -0500

    update

--- mvp.py
+++ mvp.py
@@ -21,9 +21,12 @@ while True:
 import pandas as pd
 
 # Leer el archivo CSV
-df = pd.read_csv('products_curated.csv')  # Asegúrate de que la ruta al archivo CSV sea correcta
+df = pd.read_csv('products_curated_updated.csv')  # Asegúrate de que la ruta al archivo CSV sea correcta
 
-# Iterar sobre las filas del DataFrame
+# Crear un nuevo DataFrame para almacenar los resultados
+sales_pitch_df = pd.DataFrame(columns=['codsap', 'sales_pitch'])
+
+# Iterar sobre las filas del DataFrame original
 for index, row in df.iterrows():
     # Extraer los valores de las columnas específicas y asignarlas a variables
     cod = row['codsap']
@@ -32,8 +35,12 @@ for index, row in df.iterrows():
     brand = row['desmarca']
     category = row['descategoria']
     
-    # Aquí puedes hacer lo que necesites con las variables
-    print(cod, product, group, brand, category)
+    # Obtener el sales pitch y la respuesta
     formatted_messages = SalesPitchBuilder.get_sales_pitch(product, group, category, brand)
     response = OpenAIChat.get_response(formatted_messages)
-    print(response)
+    
+    # Añadir una nueva fila al DataFrame de sales_pitch
+    sales_pitch_df = sales_pitch_df.append({'codsap': cod, 'sales_pitch': response}, ignore_index=True)
+
+# Guardar el DataFrame de sales_pitch en un nuevo archivo CSV
+sales_pitch_df.to_csv('sales_pitch.csv', index=False, encoding='utf-8-sig')
